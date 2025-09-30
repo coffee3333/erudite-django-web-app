3f6435fda6ca0d9b1b79612027f27a6fa29a2f23
Atai Mamytov <atay.mamytov@gmail.com>
2025-09-30 14:58:24 +0200

    KAN-18 Create a registration, added a view for a regestration, also a serializer for it

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -9,3 +9,45 @@ class LoginSerializer(serializers.Serializer):
 
 class LogoutSerializer(serializers.Serializer):
     refresh_token = serializers.CharField()
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True, label="Confirm password")
+    user_bio = serializers.CharField(required=False, allow_blank=True)
+    photo = serializers.ImageField(required=False, allow_null=True)
+    role = serializers.ChoiceField(choices=User.ROLE_CHOICES, default="student")
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password', 'password2', 'user_bio', 'photo', 'role')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password'],
+            user_bio=validated_data.get('user_bio', ''),
+            photo=validated_data.get('photo', None),
+            role = validated_data.get("role", "student")
+        )
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'user_bio', 'photo', 'date_joined', 'slug', 'role')
+        ref_name = "AuthUserSerializer"
