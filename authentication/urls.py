4ae243714ad0586e6c88109bfde02ec29517e3f6
Atai Mamytov <atay.mamytov@gmail.com>
2025-10-01 10:59:11 +0200

    KAN-20 Create user token refresh

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from authentication.views.login_view import LoginView, LogoutView
 from authentication.views.register_view import RegisterView
 from authentication.views.google_oauth import GoogleExchangeView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     # Authentication
@@ -14,4 +15,7 @@ urlpatterns = [
     # Google O-Auth
     path("oauth/", include("social_django.urls", namespace="social")),
     path("auth/google/", GoogleExchangeView.as_view()),
+
+    # Token Refresh
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
