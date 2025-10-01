c749831cd1c52f53e831dcc83f4f9a0505040fed
Atai Mamytov <atay.mamytov@gmail.com>
2025-10-01 10:55:50 +0200

    KAN-19 Create a o-auth end point for google 2fa

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 
 from authentication.views.login_view import LoginView, LogoutView
 from authentication.views.register_view import RegisterView
+from authentication.views.google_oauth import GoogleExchangeView
 
 urlpatterns = [
     # Authentication
@@ -9,4 +10,8 @@ urlpatterns = [
     path('auth/logout/', LogoutView.as_view(), name='logout'),
 
     path('auth/registration/', RegisterView.as_view(), name='registration'),
+
+    # Google O-Auth
+    path("oauth/", include("social_django.urls", namespace="social")),
+    path("auth/google/", GoogleExchangeView.as_view()),
 ]
